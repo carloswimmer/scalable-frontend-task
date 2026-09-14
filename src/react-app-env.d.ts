594d07222a3c43386f0dc18b20eeff4dd56f0dde
Carlos Wimmer <coiote777@gmail.com>
2026-09-15 01:42:06 +0200

    chore: add GraphQL env templates

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="react-scripts" />
 
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_GRAPHQL_BASE_URL?: string;
+    REACT_APP_GRAPHQL_MOCK?: string;
+  }
+}
+
 declare module "*.css"
